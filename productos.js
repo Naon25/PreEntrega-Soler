af87f04296581e2e1edadef50ff1d07e5fe13e69
Leandro Soler <leandrosoler.7@gmail.com>
2022-11-26 12:58:09 -0300

    Pre entrega lista

--- productos.js
+++
@@ -1,8 +0,0 @@
-class Producto {
-    constructor(id, nombre, precio) {
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = Number(precio);
-    }
-}
-
---
+++ src/productos.js
@@ -0,0 +1,32 @@
+let stockProductos = 
+[
+   {
+        id: 1,
+        nombre: "vino",
+        precio: 900,
+        img: 'public/images/vino1.jpg',
+        cantidad: 1
+   },
+
+   {
+        id: 2,
+        nombre: "sanguche",
+        precio: 600,
+        img: 'public/images/sanguche.jpg',
+        cantidad: 1
+    },
+
+    {
+        id: 3,
+        nombre: "torta",
+        precio: 1200,
+        img: 'public/images/torta.jpg',
+        cantidad: 1
+    }
+]
+
+// let stockProductos = [
+//     {id: 1, nombre: "Buzo 1", tipo: "buzo", cantidad: 1, desc: "Un buzo que re va con vos", precio: 1200, talle: "L", img: './img/buzo.jpg'},
+//     {id: 2, nombre: "Buzo 2", tipo: "buzo", cantidad: 1, desc: "Un buzo que re va con vos", precio: 1100, talle: "L", img: './img/buzo.jpg'},
+//     {id: 3, nombre: "Buzo 3", tipo: "buzo", cantidad: 1, desc: "Un buzo que re va con vos", precio: 1200, talle: "M", img: './img/buzo.jpg'},
+// ]
